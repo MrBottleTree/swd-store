f663a4cac8c00ee92d192c07c08276f7957cf8d5
Vishrut Ramraj <vishrut172@gmail.com>
2026-04-19 13:25:15 +0530

    Fixed circular import errors

--- core/apps.py
+++ core/apps.py
@@ -1,11 +1,6 @@
 from django.apps import AppConfig
-from django.contrib.admin.apps import AdminConfig
 
 
 class CoreConfig(AppConfig):
     name = 'core'
     verbose_name = 'BITS Pilani Store'
-
-
-class SWDAdminConfig(AdminConfig):
-    default_site = 'core.admin.SWDAdminSite'
